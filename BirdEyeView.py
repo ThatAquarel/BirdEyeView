d4bb9cc6f09006c00693dcb809de5e3a555d284d
Aquarel <54329158+AquarelMc@users.noreply.github.com>
2020-09-26 09:53:30 -0400

    Uploaded files

---
+++ BirdEyeView.py
@@ -0,0 +1,123 @@
+import cv2 as cv
+import numpy as np
+from math import sin, tan, atan, radians, degrees
+
+boundaries = ([87, 51, 0], [255, 135, 150])
+
+camId = 0
+width = 1280
+height = 720
+
+# fill following 5 parameters with GetParameters.py
+camAngle = 61.16249506470501  # camera angle
+point1 = (290, 1)  # top left
+point2 = (976, 1)  # top right
+point3 = (1280, 696)  # bottom right
+point4 = (0, 696)  # bottom left
+
+aspectRatio = height / width
+hFov = 60
+vFov = 2 * degrees(atan(aspectRatio * tan(radians(hFov / 2))))
+
+vScale = 1
+firstIteration = True
+
+pts = np.array([point1, point2, point3, point4], dtype="float32")
+
+
+def vertical_scale_factor(fg):
+    b = camAngle
+    c = vFov
+
+    a = 90 - b - c / 2
+    d = 180 - 90 - a
+    e = (180 - c) / 2
+    f = 180 - d - e
+    g = 180 - e
+    h = 180 - f - g
+
+    fh = fg * sin(radians(g)) / sin(radians(h))
+
+    return fh / fg
+
+
+def order_points(points):
+    rect = np.zeros((4, 2), dtype="float32")
+
+    s = points.sum(axis=1)
+    rect[0] = points[np.argmin(s)]
+    rect[2] = points[np.argmax(s)]
+
+    diff = np.diff(points, axis=1)
+    rect[1] = points[np.argmin(diff)]
+    rect[3] = points[np.argmax(diff)]
+
+    return rect
+
+
+def four_point_transform(image, points):
+    global firstIteration, vScale
+
+    rect = order_points(points)
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    M = cv.getPerspectiveTransform(rect, dst)
+
+    if firstIteration:
+        firstIteration = False
+        vScale = vertical_scale_factor(maxHeight)
+
+    return cv.warpPerspective(image, M, (maxWidth, maxHeight))
+
+
+class BirdEyeView:
+    ret = None
+    frame = None
+    warped = None
+
+    def __init__(self):
+        global pts, firstIteration, vScale
+
+        cap = cv.VideoCapture(camId)
+        cap.set(3, width)
+        cap.set(4, height)
+
+        if not cap.isOpened():
+            print("Cannot open camera")
+            exit()
+
+        while True:
+            self.ret, self.frame = cap.read()
+
+            self.warped = four_point_transform(self.frame, pts)
+            self.warped = cv.resize(self.warped, None, fx=1, fy=vScale, interpolation=cv.INTER_CUBIC)
+            # resize to actual vertical size
+
+            self.warped = cv.resize(self.warped, None, fx=0.3, fy=0.3, interpolation=cv.INTER_CUBIC)
+            # resize window to fit monitor
+
+            cv.imshow('warped', self.warped)
+
+            if cv.waitKey(1) == ord('q'):
+                break
+
+        cap.release()
+        cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    GetCameraFov = GetCameraFov()
---
+++ GetParameters.py
@@ -0,0 +1,93 @@
+import cv2 as cv
+from math import tan, atan, sin, degrees
+
+width = 1280
+height = 720
+camId = 0
+
+line1 = []
+line2 = []
+
+
+def drawLine(x, y):
+    if len(line1) != 2:
+        line1.append([x, y])
+        # print("written in line 1")
+    elif len(line2) != 2:
+        line2.append([x, y])
+        # print("written in line 2")
+
+
+def calculateParameters():
+    global line1, line2
+
+    a1 = degrees(atan((line1[0][0] - line1[1][0]) / (line1[1][1] - line1[0][1])))
+    a2 = degrees(atan((line2[1][0] - line2[0][0]) / (line2[1][1] - line2[0][1])))
+    average = (a1 + a2) / 2
+    camAngle = 91.3651073333154 + (-2.28277686222745 * average) + (0.0895247387148957 * average * average) + (
+                -0.00314825231641227 * average * average * average)
+
+    pt1 = (line1[0][0] - line1[1][0], line1[0][1])
+    pt2 = (line2[0][0] + (width - line2[1][0]), line2[0][1])
+    pt3 = (width, line2[1][1])
+    pt4 = (0, line1[1][1])
+
+    print("Camera Angle = " + str(camAngle))
+
+    print("Point 1 = " + str(pt1))
+    print("Point 2 = " + str(pt2))
+    print("Point 3 = " + str(pt3))
+    print("Point 4 = " + str(pt4))
+
+    line1 = []
+    line2 = []
+
+
+def onMouse(event, x, y, flags, param):
+    if event == cv.EVENT_LBUTTONDOWN:
+        # print('Down x = %d, y = %d' % (x, y))
+        drawLine(x, y)
+    elif event == cv.EVENT_LBUTTONUP:
+        # print('Up x = %d, y = %d' % (x, y))
+        drawLine(x, y)
+
+
+class GetParameters:
+    ret = None
+    frame = None
+
+    def __init__(self):
+        cap = cv.VideoCapture(camId)
+        cap.set(3, width)
+        cap.set(4, height)
+
+        if not cap.isOpened():
+            print("Cannot open camera")
+            exit()
+
+        while True:
+            self.ret, self.frame = cap.read()
+
+            if len(line1) == 2:
+                cv.line(img=self.frame, pt1=(line1[0][0], line1[0][1]), pt2=(line1[1][0], line1[1][1]),
+                        color=(0, 255, 0), thickness=3, lineType=8, shift=0)
+            if len(line2) == 2:
+                cv.line(img=self.frame, pt1=(line2[0][0], line2[0][1]), pt2=(line2[1][0], line2[1][1]),
+                        color=(0, 0, 255), thickness=3, lineType=8, shift=0)
+                calculateParameters()
+
+                break
+
+            cv.imshow('frame', self.frame)
+
+            cv.setMouseCallback('frame', onMouse)
+
+            if cv.waitKey(1) == ord('q'):
+                break
+
+        cap.release()
+        cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    GetParameters = GetParameters()
